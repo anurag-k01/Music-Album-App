cafdc732ebb053cabefcc37e4550350801fa62f9
anurag-k01 <aayandcruzz452@gmail.com>
2022-01-14 20:52:40 +0530

    First COmmit

---
+++ music-album/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Music App</h1>
+    {/**music  */}
+    </div>
+  );
+}
+
+export default App;
