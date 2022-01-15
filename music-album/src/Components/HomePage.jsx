2f2cf83fbc79ef469cc5f3e87d4f2adec26ab0ec
anurag-k01 <aayandcruzz452@gmail.com>
2022-01-15 20:34:04 +0530

    Done Sorting FUnctionality

--- music-album/src/Components/HomePage.jsx
+++ music-album/src/Components/HomePage.jsx
@@ -1,14 +1,28 @@
 import axios from "axios";
+
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 function HomePage() {
   const [albums, setAlbums] = useState([]);
+
   const [querry, setQuerry] = useState("");
-  const [filter, setFiler] = useState(3);
+    const [filter, setFilter] = useState(3);
+    
   const handleInput = (e) => {
     setQuerry(e.target.value);
-  };
+    };
+    const filterByYear = () => {
+      if (filter == 1) {
+        setFilter(2);
+      } else if (filter == 2) {
+        setFilter(3);
+      } else {
+        setFilter(1);
+      }
+        // setFilter(1);
+        // console.log(filter)
+    };
   useEffect(() => {
     axios
       .get("http://localhost:8000/")
@@ -20,39 +34,37 @@ function HomePage() {
         console.log(err);
       });
   }, []);
+ 
+  function Data(albums) {
+    if (albums.length == 0) {
+      return <h1>No Results Found</h1>;
+    }
+
+    return albums.map((el) => (
+      <div key={el._id} onClick={() => getALbumID(el._id)}>
+        <a href="/songs">
+          <div>{el.name}</div>
+          <Image src={el.cover_photo} />
+        </a>
+      </div>
+    ));
+  }
   const getALbumID = (e) => {
     localStorage.setItem("albumId", e);
   };
-  const filterByGenre = (querry) => {
-    albums.map((el) => {
-      if (querry == el.name) {
-        return querry;
-      } else {
-        console.log("Not Found");
-      }
-    });
-  };
+   
   return (
     <>
       <Container>
         <div>
           <label>Search Album By Name: </label>
           <SearchBar value={querry} onInput={handleInput}></SearchBar>
-        
-          <button></button>
+
+                   <button onClick={filterByYear}>{filter==1?"Oldest First":filter==2?"Lateset First":"Sort By Year"}</button> 
         </div>
-        {/*<FilterByGenre>Filter By Genre</FilterByGenre*/}
+
         <Layout>
-          {albums
-            .filter((e) => (querry == "" ? true : e.name == querry))
-            .map((el) => (
-              <div key={el._id} onClick={() => getALbumID(el._id)}>
-                <a href="/songs">
-                  <div>{el.name}</div>
-                  <Image src={el.cover_photo} />
-                </a>
-              </div>
-            ))}
+          {Data(albums.filter((e) => (querry == "" ? true : e.name == querry)).sort((a,b)=>filter==1?a.year-b.year:filter==2?b.year-a.year:a.year==b.year))}
         </Layout>
       </Container>
     </>
